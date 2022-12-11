d982fa380c5eebda5a1fa8e7726f16a9ebd382b7
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-11 18:44:53 +0530

    initial

---
+++ src/main/java/com/nest/nestemployeeapp_backend/NestemployeeappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestemployeeapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestemployeeappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestemployeeappBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nest/nestemployeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.nest.nestemployeeapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class EmployeeController {
+
+
+}
